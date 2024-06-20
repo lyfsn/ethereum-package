621515669a40435404cac1a623ac682ecb7e1a53
lyfsn <dev.wangyu@proton.me>
2024-06-20 19:28:29 +0800

    up

--- otterscan/start.sh
+++ otterscan/start.sh
@@ -5,7 +5,7 @@ docker run \
   -p 9988:80 \
   --name otterscan \
   -d \
-  --env ERIGON_URL="http://95.217.233.186:33086/" \
-  --env BEACON_API_URL="http://95.217.233.186:33089/" \
+  --env ERIGON_URL="http://95.217.233.186:33102/" \
+  --env BEACON_API_URL="http://95.217.233.186:33108/" \
   otterscan/otterscan:latest
 
