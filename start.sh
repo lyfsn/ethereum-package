c9db9f8a54bededeed7c0756e90537e9580735ca
lyfsn <dev.wangyu@proton.me>
2024-06-20 14:21:46 +0800

    up

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 kurtosis run --enclave my-testnet . --args-file network_params.yaml
+
+
+
+
 # kurtosis run --enclave my-testnet github.com/kurtosis-tech/ethereum-package --args-file network_params.yaml
+
+
+
+
+# kurtosis run --enclave my-testnet github.com/ethpandaops/ethereum-package --args-file network_params.yaml
+
